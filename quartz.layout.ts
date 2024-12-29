cb145773cd6126445aa093336527a94762c94f0b
Rukius <rukius889@gmail.com>
2024-12-29 20:19:13 +0100

    Quartz sync: Dec 29, 2024, 8:19 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -30,10 +30,10 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
+  footer: Component.Graph(),
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
@@ -47,4 +47,5 @@ export const defaultListPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [],
+  footer: Component.Graph(),
 }
